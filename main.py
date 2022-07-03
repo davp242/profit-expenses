51b12291a2d9834b3b46fc9d03adef8d24344af6
david <davidaperry607@gmail.com>
2022-07-02 19:00:15 -0700

    updated goals for tomorrow

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
-TEST
+# Goals for Sunday: make menu function with options to enter new profit or new expense. Probably will try
+# using nested lists? ie profit[[name, address, flt(dollar amount)][name, address, flt(dollar amount)]] etc.
+# to track them. Create functions for profit and expense entries and focus on getting them to correctly
+# update the data sets, focus on getting the values to save to something simple like a .txt file eventually
+# and worry about moving it to a spreadsheet later on. Still afraid to work with fancy APIs
